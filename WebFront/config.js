6a26012a89ce0c2388bafb8dca36999491dd4030
Luisa Campanha <campanhaluisa@gmail.com>
2026-08-09 18:24:21 -0300

    Delete WebFront/config.js

--- WebFront/config.js
+++
@@ -1,5 +0,0 @@
-// Este arquivo NÃO deve ser versionado (veja .gitignore).
-// Preencha com sua própria chave da API da Mistral.
-window.APP_CONFIG = {
-  MISTRAL_API_KEY: 'YqO39pccm4tnqUNQMiRqJXUypPCZPjoM',
-};
